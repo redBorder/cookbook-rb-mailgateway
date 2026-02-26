94751466dde880b995ac8d2ccdeb55b510afb56d
Rafael Gomez <rgomez@redborder.com>
2026-02-26 14:53:26 +0000

    Bump version to 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 cookbook-rb-mailgateway CHANGELOG
 ===============
 
+## 0.0.2
+
+  - vimesa
+    - [b9bce0f] First version of cookbook-rb-mailgateway
+
 ## 0.0.1
 - [Vicente Mesa] - Initial release of mailgateway
--- resources/metadata.rb
+++ resources/metadata.rb
@@ -5,7 +5,7 @@ maintainer       'Eneo Tecnología S.L.'
 maintainer_email 'git@redborder.com'
 license          'AGPL-3.0'
 description      'Installs/Configures redborder mailgateway'
-version          '0.0.1'
+version          '0.0.2'
 
 depends 'rb-common'
 depends 'rb-selinux'
